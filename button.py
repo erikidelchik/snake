bd9d161073aacf8a2b741b980aaf7f8f95c7ead1
erik <110602802+erikidelchik@users.noreply.github.com>
2023-11-17 20:33:56 +0200

    Add files via upload

---
+++ button.py
@@ -0,0 +1,34 @@
+import pygame.mouse
+
+
+class Button:
+    def __init__(self, x, y, image):
+        self.x = x
+        self.y = y
+        self.image = image
+        self.rect = self.image.get_rect()
+        self.rect.center = (x, y)
+
+    def draw(self, win):
+        win.blit(self.image, (self.rect.x, self.rect.y))
+
+    def mouse_on_button(self):
+        mouse_pos = pygame.mouse.get_pos()
+        if self.rect.collidepoint(mouse_pos):
+            if pygame.mouse.get_pressed()[0]:
+                return True
+
+
+class Image:
+
+    def __init__(self, x, y, image):
+        self.x = x
+        self.y = y
+        self.image = image
+        self.rect = self.image.get_rect()
+        self.rect.topleft = (x, y)
+
+    def draw(self, win):
+        self.image = pygame.transform.scale(self.image, (20, 20))
+        win.blit(self.image, (self.rect.x, self.rect.y))
+
